8d879ca2433597f8650916b740cd438725a5492d
Alexey Lazarev <52629691+AlexeyLz@users.noreply.github.com>
2021-04-08 23:50:17 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-//comm   новая ветка
+//comm   новая ветка  part2
 
 class _MyAppState extends State<MyApp> {
   final _questions = const [
